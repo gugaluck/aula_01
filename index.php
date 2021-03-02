c83aa71c6200e44a3968b39de6f9dd8ae2adb022
gugaluck <gustavoluckmann@live.com>
2021-03-02 13:23:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,58 @@
+<?php
+    require('autoload.php');
+    require('login.class.php');
+
+    echo new Link('http://unidavi.edu.br',
+    'Unidavi 2021',"_blank",'btn');
+    echo new Link('http://google.com.br',
+    'Google',"_blank",'btn');
+
+    $ul = new Ul();
+    $li1 = new Li('item 1', 'azul');
+    $li2 = new Li('item 2', 'azul');
+    $ul->addLi($li1);
+    $ul->addLi($li2);
+    echo $ul;
+
+    //TEM QUE FAZER O FORM TUDO EM PHP, TABELAS, METATAGS, BODY, HEADER, CLASSES
+
+    /*arrays
+    $tables = array (
+        'h1' => 'Hello, world!');
+    echo $tables['h1'];
+*/
+    //receber nome
+    if (isset ($_POST['nome'])){
+        $nome = $_POST['nome'];
+    }
+
+    if (isset ($_POST['sobrenome'])){
+        $sobrenome = $_POST['sobrenome'];
+        echo "Nome digitado: <br> $nome $sobrenome";
+
+    }
+    //receber dados
+    /*
+    if (isset ($_POST['botao'])){
+        $nome = $_POST['botao'];
+        echo "nome digitado $nome";
+    }*/
+
+
+?>
+
 <html>
     <body>
-
-        <form action="" method"post">
-            <p>
-                <label for"nome">Nome:</label><br>
-                <input type="text" name="nome" id="name"><br>
-                <input type="submit" value="botao"> 
-            </p>  
-        </form>
+        <div class="form">
+            <h1>Criar usuário</h1>
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+                <label>Nome:</label>
+                <input type="text" name="nome">
+                <br>
+                <label>Sobrenome:</label>
+                <input type="text" name="sobrenome">
+                <input type="submit" value="submit" name="botao">
+            </form>
+        </div>
     </body>
 </html>
-<?php
-require('autoload.php');
-
-echo new Link('http://unidavi.edu.br',
-'Unidavi 2021',"_blank",'btn');
-echo new Link('http://google.com.br',
-'Google',"_blank",'btn');
-
-$ul = new Ul();
-$li1 = new Li('item 1', 'azul');
-$li2 = new Li('item 2', 'azul');
-$ul->addLi($li1);
-$ul->addLi($li2);
-echo $ul;
-
-//arrays
-$tables = array (
-    'h1' => 'Hello, world!');
-echo $tables['h1'];
-//receber nome
-if (isset ($_POST['nome'])){
-    $nome = $_POST['nome'];
-    echo "nome digitado $nome";
-}
-
-
-
-//TEM QUE FAZER O FORM TUDO EM PHP, TABELAS, METATAGS, BODY, HEADER
-/*
-<div class = "index">
-    <form method ="POST" action ="index.php" class="form">
-        <p>
-            <input id="nome" name="nome" type="text"/>
-        </p>
-    </form>
-</div>
-*/
-?>
